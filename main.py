1f6e0d0a3ff6082a79ce49de56f7948c1b6a638b
Victoria <vmoix001@mail.ru>
2019-09-20 23:52:06 +0200

    Initial proj.

--- main.py
+++ main.py
@@ -1,8 +1,77 @@
 # Case- study #1
-# Developers : Moiseenko V. ,
-#              Torgasheva A.
+# Developers : Moiseenko V. , Torgasheva A. , Seckov M.
 import turtle
+def red_triangle():
+    # TODO: (Alena) Paint a red triangle.
+pass
+def yellow_triangle():
+    #TODO: (Alena) Paint a yellow triangle.
+def rhomb():
+    #TODO: (Alena) Paint a rhomb.
+pass
+def pink_triangle():
+    #TODO: (Victoria) Paint a pink triangle.
+pass
+def purple_triangle():
+    #TODO: (Victoria) Paint a purple triangle.
+pass
+def blue_triangle():
+    #TODO: (Maxim) Paint an blue triangle.
+pass
+def parallelogram():
+    #TODO: (Maxim) Paint a parallelogram.
+pass
 
-def square  (x,y,a):
-    #TODO (Alena) ылаопыдщро
-    pass
+
+def  rabbit():
+    #TODO: (Victoria) Paint a rabbit.
+parallelogram()
+rhomb()
+red_triangle()
+pink_triangle()
+yellow_triangle()
+blue_triangle()
+purple_triangle()
+pass
+def fish():
+    #TODO: (Victoria) Paint a fish.
+red_triangle()
+rhomb()
+parallelogram()
+blue_triangle()
+pink_triangle()
+purple_triangle()
+yellow_triangle()
+pass
+def spaceship():
+    #TODO: (Victoria) Paint a spaceship.
+purple_triangle()
+blue_triangle()
+yellow_triangle()
+red_triangle()
+rhomb()
+parallelogram()
+purple_triangle()
+pass
+def helicopter():
+    #TODO: (Alena) Paint a helicopter.
+blue_triangle()
+parallelogram()
+yellow_triangle()
+red_triangle()
+rhomb()
+pink_triangle()
+purple_triangle()
+pass
+def cock():
+    #TODO: (Alena) Paint a cock.
+
+
+
+
+
+
+
+
+
+
