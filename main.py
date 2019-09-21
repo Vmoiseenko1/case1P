118e8baf10a4456b88a4f1ea9bf1aa0cccc70bc3
ASUS <in-fa@mail.ru>
2019-09-21 10:04:32 +0700

    + 3 figures (2 triangles, rhomb).

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Case- study #1
+# Case - study #1
 # Developers : Moiseenko V. (%),
 #              Torgasheva A. (%),
 #              Seckov M. (%)
@@ -55,71 +55,115 @@ def rhomb (x, y, z, color):
     turtle.end_fill()
     #TODO: (Alena) Paint a rhomb.
 
-def pink_triangle():
-    #TODO: (Victoria) Paint a pink triangle.
-pass
-def purple_triangle():
+def pink_triangle ():
+    # TODO: (Victoria) Paint a pink triangle.
+    pass
+
+def purple_triangle ():
     #TODO: (Victoria) Paint a purple triangle.
-pass
-def blue_triangle():
-    #TODO: (Maxim) Paint an blue triangle.
-pass
-def parallelogram():
-    #TODO: (Maxim) Paint a parallelogram.
-pass
+    pass
+
+def blue_triangle ():
+    # TODO: (Maxim) Paint an blue triangle.
+    pass
+
+def parallelogram ():
+    # TODO: (Maxim) Paint a parallelogram.
+    pass
 
 # Pictures:
-def  rabbit():
-    #TODO: (Victoria) Paint a rabbit.
-parallelogram()
-rhomb()
-red_triangle()
-pink_triangle()
-yellow_triangle()
-blue_triangle()
-purple_triangle()
-pass
+def  rabbit ():
+    # TODO: (Victoria) Paint a rabbit.
+    # parallelogram()
+    # rhomb()
+    # red_triangle()
+    # pink_triangle()
+    # yellow_triangle()
+    # blue_triangle()
+    # purple_triangle()
+    pass
 
-def fish():
-    #TODO: (Victoria) Paint a fish.
-red_triangle()
-rhomb()
-parallelogram()
-blue_triangle()
-pink_triangle()
-purple_triangle()
-yellow_triangle()
-pass
+def fish ():
+    # TODO: (Victoria) Paint a fish.
+    # red_triangle()
+    # rhomb()
+    # parallelogram()
+    # blue_triangle()
+    # pink_triangle()
+    # purple_triangle()
+    # yellow_triangle()
+    pass
 
-def spaceship():
-    #TODO: (Victoria) Paint a spaceship.
-purple_triangle()
-blue_triangle()
-yellow_triangle()
-red_triangle()
-rhomb()
-parallelogram()
-purple_triangle()
-pass
+def spaceship ():
+    # TODO: (Victoria) Paint a spaceship.
+    # purple_triangle()
+    # blue_triangle()
+    # yellow_triangle()
+    # red_triangle()
+    # rhomb()
+    # parallelogram()
+    # purple_triangle()
+    pass
 
-def helicopter():
-    #TODO: (Alena) Paint a helicopter.
-blue_triangle()
-parallelogram()
-yellow_triangle()
-red_triangle()
-rhomb()
-pink_triangle()
-purple_triangle()
-pass
+def helicopter ():
+    # TODO: (Alena) Paint a helicopter.
+    # blue_triangle()
+    # parallelogram()
+    # yellow_triangle()
+    # red_triangle()
+    # rhomb()
+    # pink_triangle()
+    # purple_triangle()
+    pass
 
-def cock():
-    #TODO: (Alena) Paint a cock.
+def cock ():
+    # TODO: (Alena) Paint a cock.
+    # blue_triangle
+    # red_triangle
+    # yellow_triangle
+    # purple_triangle
+    # pink_triangle
+    # orange_rhomb
+    # green_parallelogram
+    pass
 
+def  left_running_man ():
+    # TODO: (Alena) Paint a running man.
+    # orange_rhomb
+    # red_triangle
+    # yellow_triangle
+    # blue_triangle
+    # purple_triangle
+    # pink_triangle
+    # green_parallelogram
+    pass
 
+def right_running_man ():
+    # TODO: (Maxim) Paint a running man.
+    # orange_rhomb
+    # red_triangle
+    # yellow_triangle
+    # blue_triangle
+    # purple_triangle
+    # pink_triangle
+    # green_parallelogram
+    pass
 
+def ship ():
+    # TODO: (Maxim) Paint ship.
+    # orange_rhomb
+    # red_triangle
+    # yellow_triangle
+    # blue_triangle
+    # purple_triangle
+    # pink_triangle
+    # green_parallelogram
+    pass
 
+def main ()^
+    pass
 
+main()
 
 
 
